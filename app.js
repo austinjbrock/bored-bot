0b1980a2e117d44c9a235d2841c7f2364fd7edfd
Austin Brock <austinjbrock@gmail.com>
2026-06-21 15:37:50 -0700

    button click now clears previous idea instead of stacking ideas

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ let ideaBox = document.getElementById('idea-box')
 
 button.addEventListener('click',()=>{
 
+   ideaBox.textContent = '';
+
    fetch ('https://apis.scrimba.com/bored/api/activity')
    .then(Response => Response.json())
    .then(data =>{
       ideaBox.textContent += data.activity
    })
+
 
 })
